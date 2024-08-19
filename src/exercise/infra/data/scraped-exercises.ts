393b1ae593065a75897a51ac118c9f2447390261
alvaro <alvaromagu2002@gmail.com>
2024-08-19 20:15:38 +0200

    feat: use github gists to store exercise info json

--- src/exercise/infra/data/scraped-exercises.ts
+++ src/exercise/infra/data/scraped-exercises.ts
@@ -1,29 +1,31 @@
 import { type ScrapedExercise } from '@/exercise/domain/types/scraped-exercise'
-import { readFile } from 'node:fs/promises'
 
-export const scrapedExercises = readFile('./public/exercise-info.json', 'utf-8').then(JSON.parse).then<ScrapedExercise[]>((baseExercises: any) => {
-  return baseExercises.map((baseExercise: any) => {
-    const exercises = baseExercise.exercises ?? []
-    const translation = exercises.find(({ language }: any) => language === 2) ?? exercises[0]
-    if (translation == null) {
-      return null
-    }
-    return {
-      id: baseExercise.id,
-      name: translation.name,
-      description: translation.description,
-      muscles: (baseExercise.muscles ?? []).map((muscle: any) => ({
-        id: muscle.id,
-        name: muscle.name
-      })),
-      images: (baseExercise.images ?? []).map((image: any) => ({
-        id: image.id,
-        src: image.image
-      })),
-      videos: (baseExercise.videos ?? []).map((video: any) => ({
-        id: video.id,
-        src: video.video
-      }))
-    } satisfies ScrapedExercise
-  }).filter(Boolean)
-})
+export const scrapedExercises =
+  fetch('https://gist.githubusercontent.com/alvaromagu/91863b6cd1dfd831a31099d840359b8a/raw/25d0227fcc442df3cbd015be5494bfeb78509ca6/exercise-info.json', {
+    cache: 'no-store'
+  }).then(async res => await res.json()).then<ScrapedExercise[]>((baseExercises: any) => {
+    return baseExercises.map((baseExercise: any) => {
+      const exercises = baseExercise.exercises ?? []
+      const translation = exercises.find(({ language }: any) => language === 2) ?? exercises[0]
+      if (translation == null) {
+        return null
+      }
+      return {
+        id: baseExercise.id,
+        name: translation.name,
+        description: translation.description,
+        muscles: (baseExercise.muscles ?? []).map((muscle: any) => ({
+          id: muscle.id,
+          name: muscle.name
+        })),
+        images: (baseExercise.images ?? []).map((image: any) => ({
+          id: image.id,
+          src: image.image
+        })),
+        videos: (baseExercise.videos ?? []).map((video: any) => ({
+          id: video.id,
+          src: video.video
+        }))
+      } satisfies ScrapedExercise
+    }).filter(Boolean)
+  })
